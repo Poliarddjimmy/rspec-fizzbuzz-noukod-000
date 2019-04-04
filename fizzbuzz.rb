d454ce9bda06868a7f76d089511a18fcd6875c63
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-04 19:52:35 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(fiz_3)
+  if fiz_3 % 3 == 0 && fiz_3 % 5 == 0 
+    return "FizzBuzz"
+  end 
+  
+  if fiz_3 % 3 == 0 
+    return "Fizz"
+  end
+  
+  if fiz_3 % 5 == 0 
+    return "Buzz"
+  end
+end
