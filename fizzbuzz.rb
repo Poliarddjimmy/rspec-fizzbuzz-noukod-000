bb982acef3cf70cd52d60989f20e677989357a20
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-11 15:55:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,6 +3,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(fiz_3)
   if fiz_3 % 3 == 0 && fiz_3 % 5 == 0 
+<<<<<<< HEAD
     return "FizzBuzz"
   end 
   
@@ -13,4 +14,14 @@ def fizzbuzz(fiz_3)
   if fiz_3 % 5 == 0 
     return "Buzz"
   end
+=======
+    "FizzBuzz"
+  end 
+  
+  if fiz_3 % 3 == 0 
+    "Fizz"
+  end
+  
+  if 
+>>>>>>> 144ed0a69eb91adfc8feddd43b55f27e0c842d64
 end
